b698866d7218ad60fb4c4bc959f49c24f699d4cb
adjonata <alexdjonata30@gmail.com>
2021-03-24 18:56:17 -0300

    feat: nuxt frontend project

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">Alex Djonata Portfólio</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+@import '@/styles/_colors.scss';
+
+.container {
+  color: $purple;
+}
+</style>
